5324a3c045d0edf1211c143ec6df1d4fcc5af515
arrivaldo <erickbarcena00@hotmail.com>
2024-10-26 09:47:25 -0600

    Notes server url reset commit

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/note', noteRouter)
 
+const PORT = process.env.PORT || 5000; // Use the PORT environment variable or default to 5000
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectToMongoDB()
-    console.log("Server is running")
+    console.log(`Server is running on port ${PORT}`);
 })
